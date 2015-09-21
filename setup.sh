090a02c8e4784e2b6e6161b0aee4179312c8fc7c
sadah <sadah@gp-sadah.local>
2015-09-21 22:57:22 +0900

    rename shell script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+ln -s ~/git/dotfiles/dot.alias ~/.alias
+ln -s ~/git/dotfiles/dot.pryrc ~/.pryrc
+ln -s ~/git/dotfiles/dot.tigrc ~/.tigrc
+ln -s ~/git/dotfiles/dot.tmux.conf ~/.tmux.conf
+ln -s ~/git/dotfiles/dot.vimrc ~/.vimrc
+ln -s ~/git/dotfiles/dot.zshrc ~/.zshrc
