b98fed3b04d43a0c105940373cba6f80679379bd
sadah <sadah@gp-sadah.local>
2015-09-21 21:38:18 +0900

    update linker

--- link.sh
+++ link.sh
@@ -1,2 +1,6 @@
-ln -s ./dot.zshrc ~/.zshrc
+ln -s ./dot.alias ~/.alias
+ln -s ./dot.pryrc ~/.pryrc
+ln -s ./dot.tigrc ~/.tigrc
 ln -s ./dot.tmux.conf ~/.tmux.conf
+ln -s ./dot.vimrc ~/.vimrc
+ln -s ./dot.zshrc ~/.zshrc
