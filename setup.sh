0d0db1a51d502a0bb67d55515fa5ef186b7c4812
sadah <sadah@SadacBookPro.local>
2021-08-22 18:22:41 +0900

    Update dotfiles
    * Change default shell from zsh to fish
    * Remove unnecessary files

--- setup.sh
+++ setup.sh
@@ -1,10 +1,26 @@
-mkdir ~/.vimbackup
-mkdir -p ~/.vim/bundle
-ln -s ~/git/dotfiles/dot.alias ~/.alias
-ln -s ~/git/dotfiles/dot.pryrc ~/.pryrc
-ln -s ~/git/dotfiles/dot.tigrc ~/.tigrc
-ln -s ~/git/dotfiles/dot.tmux.conf ~/.tmux.conf
-ln -s ~/git/dotfiles/dot.vimrc ~/.vimrc
-ln -s ~/git/dotfiles/dot.zshrc ~/.zshrc
-ln -s ~/git/dotfiles/dot.gitconfig ~/.gitconfig
-ln -s ~/git/dotfiles/dot.gitignore ~/.gitignore
+#!/bin/bash
+
+# Show invisible files. Need to restart OS
+defaults write com.apple.finder AppleShowAllFiles TRUE
+
+# Install Command Line Tools
+xcode-select --install
+
+# Create links
+dotfiles_path=$(cd $(dirname $0) && pwd)
+ln -sfn ${dotfiles_path}/fish ~/.config/fish
+ln -sfn ${dotfiles_path}/.alias ~/.alias
+ln -sfn ${dotfiles_path}/.Brewfile ~/.Brewfile
+ln -sfn ${dotfiles_path}/.gitconfig ~/.gitconfig
+ln -sfn ${dotfiles_path}/.gitignore ~/.gitignore
+
+# Homebrew
+which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+which brew >/dev/null 2>&1 && brew doctor
+which brew >/dev/null 2>&1 && brew update --verbose
+which brew >/dev/null 2>&1 && brew bundle --global --verbose
+
+# fish
+which fish >/dev/null 2>&1 || curl -L https://get.oh-my.fish | fish
+omf install https://github.com/jhillyerd/plugin-git
+omf install bobthefish
